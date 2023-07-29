5293317103502749d98c5f10640ef2786ab2412f
vp337247 <vishnu337247@gmail.com>
2023-07-29 10:56:53 +0530

    page Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,223 @@
-import Image from 'next/image'
+'use client'
+// page.tsx
+import React from 'react';
+import { usePageHelper, PageHelperProps } from './pageHelpers';
 
 export default function Home() {
+  // Get the functions and state variables from pageHelper.tsx
+  const pageHelperProps: PageHelperProps = usePageHelper();
+
+  // Destructure the required functions and state variables from pageHelperProps
+  const {
+    csvData,
+    validationErrors,
+    isFileUploaded,
+    fileType,
+    showErrorMessage,
+    isSubmitted,
+    showTable,
+    shouldShowTable,
+    editingRowIndex,
+    editedRowData,
+    deletingRowIndex,
+    setEditedRowData,
+    handleEditRow,
+    handleSaveEdit,
+    handleCancelEdit,
+    handleUpdateRow,
+    handleOpenDeleteConfirmation,
+    handleCloseDeleteConfirmation,
+    handleConfirmDelete,
+    handleFileChange,
+    handleDownload,
+    handleSubmit,
+    handleAddRow,
+  } = pageHelperProps;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="flex flex-col items-center justify-start h-screen bg-zinc-950">
+      <h1 className="mb-10 text-3xl font-bold pt-3 text-white-800">CSV Validator</h1>
+      {/*Filepicker*/}
+      <div className="w-3/4 mb-3">
+        <label htmlFor="formFile" className="block text-black-700 font-medium mb-1">
+          Upload CSV file
+        </label>
+        <input
+          className="w-full px-3 py-2 border border-blue-300 bg-black rounded-md focus:outline-none focus:ring focus:ring-blue-200 focus:border-blue-500"
+          type="file"
+          id="formFile"
+          onChange={handleFileChange}
         />
+        {isFileUploaded && !showErrorMessage && (
+          <button
+            className="mt-3 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600 transition-all duration-300"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        )}
       </div>
+      {/*Filepicker Error Handler */}
+      {showErrorMessage && (
+        <div className="w-3/4 mb-3">
+          <p className="text-red-600">Please upload a CSV file. The selected file type is: {fileType}</p>
+        </div>
+      )}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      {isSubmitted && csvData.length > 0 && (
+        <>
+          <table className="w-3/4 border-collapse border border-gray-500">
+            {/* Table Header */}
+            <thead>
+              <tr>
+                <th className="border border-blue-300 p-2">Name</th>
+                <th className="border border-blue-300 p-2">Phone Number</th>
+                <th className="border border-blue-300 p-2">Address</th>
+                <th className="border border-blue-300 p-2">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Table Rows */}
+              {csvData.map((row, index) => (
+                <tr key={index}>
+                  <td className={`border hover:bg-blue-900 border-blue-300  p-2 `}>
+                    {row.Name}
+                    {!validationErrors[index].name && (
+                      <div className="text-red-500">{validationErrors[index].nameErrorMessage}</div>
+                    )}
+                  </td>
+                  <td className={`border border-blue-300 hover:bg-blue-900 p-2 `}>
+                    {row['Phone Number']}
+                    {!validationErrors[index].phone && (
+                      <div className="text-red-500">{validationErrors[index].phoneErrorMessage}</div>
+                    )}
+                  </td>
+                  <td className="border border-blue-300 hover:bg-blue-900 p-2">{row.Address}</td>
+                  <td className="border border-blue-300 p-2">
+                    <button
+                      className="mr-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                      onClick={() => handleEditRow(index)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                      onClick={() => handleOpenDeleteConfirmation(index)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          {/*Download Button */}
+          <div className="w-3/4 mt-3 flex justify-start">
+            <button
+              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600 transition-all duration-300"
+              onClick={handleDownload}
+            >
+              Download
+            </button>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          {/* Edit Modal */}
+          {editingRowIndex !== null && (
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+              <div className="bg-white p-4 w-96">
+                <h2 className="text-lg text-black font-bold mb-2">Edit Row</h2>
+                <label className="block mb-2">
+                  Name:
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    className="text-black font-bold border border-gray-400 w-full px-2 py-1"
+                    value={editedRowData.Name}
+                    onChange={(e) => setEditedRowData({ ...editedRowData, Name: e.target.value })}
+                  />
+                  {!validationErrors[editingRowIndex].name && (
+                    <div className="text-red-500">{validationErrors[editingRowIndex].nameErrorMessage}</div>
+                  )}
+                </label>
+                <label className="block mb-2">
+                  Phone Number:
+                  <input
+                    type="text"
+                    placeholder="Phone Number"
+                    className="border text-black font-bold border-gray-400 w-full px-2 py-1"
+                    value={editedRowData['Phone Number']}
+                    onChange={(e) => setEditedRowData({ ...editedRowData, 'Phone Number': e.target.value })}
+                  />
+                  {!validationErrors[editingRowIndex].phone && (
+                    <div className="text-red-500">{validationErrors[editingRowIndex].phoneErrorMessage}</div>
+                  )}
+                </label>
+                <label className="block mb-2">
+                  Address:
+                  <input
+                    type="text"
+                    placeholder="Address"
+                    className="border border-gray-400 w-full px-2 text-black font-bold py-1"
+                    value={editedRowData.Address}
+                    onChange={(e) => setEditedRowData({ ...editedRowData, Address: e.target.value })}
+                  />
+                </label>
+                <div className="flex justify-end">
+                  <button
+                    className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 mr-2"
+                    onClick={handleSaveEdit}
+                  >
+                    Save
+                  </button>
+                  <button
+                    className="px-4 py-2  bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400"
+                    onClick={handleCancelEdit}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </>
+      )}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+      {/* Add New Row Button */}
+      {isSubmitted && (
+        <div className="w-3/4 flex justify-end">
+          <button
+            className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-800 focus:outline-none focus:bg-green-600 transition-all duration-300"
+            onClick={handleAddRow}
+          >
+            Add New Row
+          </button>
+        </div>
+      )}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      {/* Delete Confirmation Modal */}
+      {deletingRowIndex !== null && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-4 w-96">
+            <h2 className="text-lg font-semibold mb-2">Confirm Delete</h2>
+            <p className="mb-4 text-black font-semibold">Are you sure you want to delete this row?</p>
+            <div className="flex justify-end">
+              <button
+                className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 mr-2"
+                onClick={handleConfirmDelete}
+              >
+                Confirm
+              </button>
+              <button
+                className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400"
+                onClick={handleCloseDeleteConfirmation}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
---
+++ src/app/pageHelpers.tsx
@@ -0,0 +1,314 @@
+'use client'
+// pageHelpers.tsx
+import React, { useState, useEffect } from 'react';
+import Papa from 'papaparse';
+
+// Define the PageHelperProps interface for type checking
+export interface PageHelperProps {
+  // State variables
+  csvData: any[];
+  setCsvData: React.Dispatch<React.SetStateAction<any[]>>;
+  validationErrors: {
+    name: boolean;
+    phone: boolean;
+    nameErrorMessage: string;
+    phoneErrorMessage: string;
+  }[];
+  setValidationErrors: React.Dispatch<
+    React.SetStateAction<{
+      name: boolean;
+      phone: boolean;
+      nameErrorMessage: string;
+      phoneErrorMessage: string;
+    }[]>
+  >;
+  isFileUploaded: boolean;
+  setIsFileUploaded: React.Dispatch<React.SetStateAction<boolean>>;
+  fileType: string;
+  setFileType: React.Dispatch<React.SetStateAction<string>>;
+  showErrorMessage: boolean;
+  setShowErrorMessage: React.Dispatch<React.SetStateAction<boolean>>;
+  isSubmitted: boolean;
+  setIsSubmitted: React.Dispatch<React.SetStateAction<boolean>>;
+  showTable: boolean;
+  setShowTable: React.Dispatch<React.SetStateAction<boolean>>;
+  shouldShowTable: boolean;
+  setShouldShowTable: React.Dispatch<React.SetStateAction<boolean>>;
+  editingRowIndex: number | null;
+  setEditingRowIndex: React.Dispatch<React.SetStateAction<number | null>>;
+  editedRowData: any;
+  setEditedRowData: React.Dispatch<React.SetStateAction<any>>;
+  deletingRowIndex: number | null;
+  setDeletingRowIndex: React.Dispatch<React.SetStateAction<number | null>>;
+
+  // Functions
+  handleEditRow: (index: number) => void;
+  handleSaveEdit: () => void;
+  handleCancelEdit: () => void;
+  handleUpdateRow: (index: number, updatedData: any) => void;
+  handleOpenDeleteConfirmation: (index: number) => void;
+  handleCloseDeleteConfirmation: () => void;
+  handleConfirmDelete: () => void;
+  handleFileChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  validateData: (data: any[]) => void;
+  handleDownload: () => void;
+  handleSubmit: () => void;
+  handleAddRow: () => void;
+}
+
+export const usePageHelper = (): PageHelperProps => {
+  const [csvData, setCsvData] = useState<any[]>([]);
+  const [validationErrors, setValidationErrors] = useState<
+    { name: boolean; phone: boolean; nameErrorMessage: string; phoneErrorMessage: string }[]
+  >([]);
+
+  const [isFileUploaded, setIsFileUploaded] = useState(false);
+  const [fileType, setFileType] = useState('');
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [showTable, setShowTable] = useState(false);
+  const [shouldShowTable, setShouldShowTable] = useState(false);
+
+  // State to keep track of the currently edited row index and data
+  const [editingRowIndex, setEditingRowIndex] = useState<number | null>(null);
+  const [editedRowData, setEditedRowData] = useState<any>({
+    Name: '',
+    'Phone Number': '',
+    Address: '',
+  });
+
+  // State to keep track of the row index to be deleted
+  const [deletingRowIndex, setDeletingRowIndex] = useState<number | null>(null);
+
+  // Function to handle editing a row
+  const handleEditRow = (index: number) => {
+    setEditingRowIndex(index);
+    setEditedRowData(csvData[index]);
+  };
+
+  // Function to save the edited row
+  const handleSaveEdit = () => {
+    if (editingRowIndex !== null) {
+      // Make a copy of the current data and update the edited row
+      const updatedCsvData = [...csvData];
+      updatedCsvData[editingRowIndex] = editedRowData;
+      setCsvData(updatedCsvData);
+
+      // Close the modal
+      setEditingRowIndex(null);
+      setEditedRowData({
+        Name: '',
+        'Phone Number': '',
+        Address: '',
+      });
+
+      // Revalidate the edited data
+      validateData(updatedCsvData);
+    }
+  };
+
+  // Function to cancel editing a row
+  const handleCancelEdit = () => {
+    setEditingRowIndex(null);
+    setEditedRowData({
+      Name: '',
+      'Phone Number': '',
+      Address: '',
+    });
+  };
+
+  // Function to update a row with new data
+  const handleUpdateRow = (index: number, updatedData: any) => {
+    const updatedCsvData = [...csvData];
+    updatedCsvData[index] = updatedData;
+    setCsvData(updatedCsvData);
+    validateData(updatedCsvData);
+  };
+
+  // Function to open the delete confirmation modal
+  const handleOpenDeleteConfirmation = (index: number) => {
+    setDeletingRowIndex(index);
+  };
+
+  // Function to close the delete confirmation modal
+  const handleCloseDeleteConfirmation = () => {
+    setDeletingRowIndex(null);
+  };
+
+  // Function to confirm the deletion of a row
+  const handleConfirmDelete = () => {
+    if (deletingRowIndex !== null) {
+      const updatedCsvData = [...csvData];
+      updatedCsvData.splice(deletingRowIndex, 1);
+      setCsvData(updatedCsvData);
+      validateData(updatedCsvData);
+      setDeletingRowIndex(null);
+    }
+  };
+
+  // Function to handle file input change
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      setIsFileUploaded(true);
+      const fileType = file.type;
+      if (fileType !== 'text/csv') {
+        setFileType(fileType);
+        setShowErrorMessage(true);
+        return;
+      }
+      setShowErrorMessage(false);
+
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        const csvText = event.target?.result as string;
+        const parsedData = Papa.parse(csvText, {
+          header: true,
+          skipEmptyLines: true, // This will skip empty lines during parsing
+        });
+
+        if (!parsedData.data || !Array.isArray(parsedData.data)) {
+          console.error('Invalid CSV data format.');
+          return;
+        }
+
+        setCsvData(parsedData.data);
+        validateData(parsedData.data);
+        setIsSubmitted(false); // Reset isSubmitted when a new file is selected
+      };
+      reader.readAsText(file);
+    }
+  };
+
+  // Function to validate the data and update the validation errors
+  const validateData = (data: any[]) => {
+    const filteredData = data.filter(
+      (row) => row.Name.trim() !== '' && row['Phone Number'].trim() !== '' && row.Address.trim() !== ''
+    );
+
+    const validationErrors = filteredData.map((row) => {
+      // Name Validation
+      const nameRegex = /^[^0-9!@#$%^&*()_+={}|[\]\\:';"<>?,./`~]*$/;
+      const isValidName = row.Name.trim().length >= 3 && nameRegex.test(row.Name);
+
+      // Phone Number Validation
+      const isValidPhone = /^[6-9]\d{9}$/.test(row['Phone Number']);
+
+      // Custom Error Messages
+      const nameErrorMessage = [];
+      if (row.Name.trim() === '') {
+        nameErrorMessage.push('Name is required');
+      }
+      if (!nameRegex.test(row.Name)) {
+        nameErrorMessage.push('Name should not contain any special characters');
+      }
+      if (row.Name.trim().length < 3) {
+        nameErrorMessage.push('Name should contain a minimum of 3 characters');
+      }
+
+      const phoneErrorMessage = [];
+      if (!isValidPhone) {
+        phoneErrorMessage.push('The phone number should be a valid Indian phone number');
+      }
+
+      return {
+        name: isValidName,
+        phone: isValidPhone,
+        nameErrorMessage: isValidName ? '' : nameErrorMessage.join(', '),
+        phoneErrorMessage: isValidPhone ? '' : phoneErrorMessage.join(', '),
+      };
+    });
+
+    setCsvData(filteredData);
+    setValidationErrors(validationErrors);
+  };
+
+  // Function to handle download of the edited data as a CSV file
+  const handleDownload = () => {
+    const csvContent = Papa.unparse(csvData);
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'edited_data.csv');
+    document.body.appendChild(link);
+    link.click();
+  };
+
+  // Function to handle the submit button click
+  const handleSubmit = () => {
+    if (!isFileUploaded) {
+      alert('Please upload a CSV file.');
+      return;
+    }
+    setIsSubmitted(true);
+    setShowTable(true);
+  };
+
+  // Effect to update the visibility of the table when isSubmitted is true
+  useEffect(() => {
+    if (isSubmitted) {
+      setShowTable(true);
+      setShouldShowTable(true);
+    }
+  }, [isSubmitted]);
+
+  // Function to add a new row to the CSV data
+  const handleAddRow = () => {
+    setCsvData((prevData) => [
+      ...prevData,
+      {
+        Name: '',
+        'Phone Number': '',
+        Address: '',
+      },
+    ]);
+    setValidationErrors((prevErrors) => [
+      ...prevErrors,
+      {
+        name: true,
+        phone: true,
+        nameErrorMessage: '',
+        phoneErrorMessage: '',
+      },
+    ]);
+  };
+
+  // Return the required state variables and functions
+  return {
+    csvData,
+    setCsvData,
+    validationErrors,
+    setValidationErrors,
+    isFileUploaded,
+    setIsFileUploaded,
+    fileType,
+    setFileType,
+    showErrorMessage,
+    setShowErrorMessage,
+    isSubmitted,
+    setIsSubmitted,
+    showTable,
+    setShowTable,
+    shouldShowTable,
+    setShouldShowTable,
+    editingRowIndex,
+    setEditingRowIndex,
+    editedRowData,
+    setEditedRowData,
+    deletingRowIndex,
+    setDeletingRowIndex,
+    handleEditRow,
+    handleSaveEdit,
+    handleCancelEdit,
+    handleUpdateRow,
+    handleOpenDeleteConfirmation,
+    handleCloseDeleteConfirmation,
+    handleConfirmDelete,
+    handleFileChange,
+    validateData,
+    handleDownload,
+    handleSubmit,
+    handleAddRow,
+  };
+};
